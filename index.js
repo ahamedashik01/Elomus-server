cc47e1d9294839b935bd63870b5ba75f18c6f608
ahamedashik01 <ashikshamim.007@gmail.com>
2021-11-18 20:03:10 +0600

    backend api post get delete added

---
+++ index.js
@@ -0,0 +1,101 @@
+const { MongoClient } = require('mongodb');
+const express = require('express');
+var cors = require('cors');
+require('dotenv').config();
+
+const ObjcetId = require('mongodb').ObjectId;
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// uri 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5cdlp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+// client 
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('client connected');
+
+        const database = client.db('elomus');
+        //PRODUCT DATA 
+        const productCollection = database.collection('products');
+        //ORDER DATA 
+        const orderCollection = database.collection('orders')
+
+
+        //PRODUCT DATA 
+
+        //GET 
+        app.get('/products', async (req, res) => {
+            const cursor = productCollection.find({});
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjcetId(id) };
+            const product = await productCollection.findOne(query);
+            res.json(product);
+        })
+
+        //POST 
+        app.post('/products', async (req, res) => {
+            const product = req.body;
+            const result = await productCollection.insertOne(product);
+            res.json(result);
+
+        });
+
+        //DELETE 
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjcetId(id) }
+            const result = await productCollection.deleteOne(query);
+            res.json(result);
+        })
+
+
+        //ORDER API
+
+        //POST
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.json(result);
+        });
+
+        //GET
+        app.get('/orders', async (req, res) => {
+            const cursor = orderCollection.find({});
+            const orders = await cursor.toArray();
+            res.json(orders);
+        });
+
+        //DELETE
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjcetId(id) }
+            const result = await orderCollection.deleteOne(query);
+            res.json(result);
+        })
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('ROOT OF Elomus SERVER')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
